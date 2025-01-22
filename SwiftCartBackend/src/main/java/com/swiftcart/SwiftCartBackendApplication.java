71b5451584d3de16f4a13b28577abe4804342dba
iamujjawalrajput <thesinghujjawal@gmail.com>
2025-01-22 21:40:17 +0530

    Initial commit

---
+++ SwiftCartBackend/src/main/java/com/swiftcart/SwiftCartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swiftcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCartBackendApplication.class, args);
+	}
+
+}
---
+++ SwiftCartBackend/src/test/java/com/swiftcart/SwiftCartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftCartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
